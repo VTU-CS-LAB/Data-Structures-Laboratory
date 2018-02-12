94a930254badcb9ade01902523acdc9dcbb252d1
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-02-12 17:43:50 +0530

    formatted

--- 01.ArrayOperations.c
+++ 01.ArrayOperations.c
@@ -9,12 +9,12 @@ e. Exit.
 Support the program with functions for each of the above operations
 */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define size 20
 
 void insert(int arr[], int *n);
-void delete(int arr[], int *n);
+void delete (int arr[], int *n);
 void display(int arr[], int n);
 
 void main()
@@ -23,26 +23,26 @@ void main()
 	printf("Enter Initial size of Array\n");
 	scanf("%d", &n);
 	printf("Enter Array Elements\n");
-	for(i=0; i<n; i++)
+	for (i = 0; i < n; i++)
 		scanf("%d", &arr[i]);
-	for(;;)
+	for (;;)
 	{
 		printf("\nMenu\n");
 		printf("1. Insert\n2. Delete\n3. Display\n4. Exit\n");
 		scanf("%d", &ch);
-		switch(ch)
+		switch (ch)
 		{
-			case 1:
-				insert(arr, &n);
-				break;
-			case 2:
-				delete(arr, &n);
-				break;
-			case 3:
-				display(arr, n);
-				break;
-			case 4:
-				exit(0);
+		case 1:
+			insert(arr, &n);
+			break;
+		case 2:
+			delete (arr, &n);
+			break;
+		case 3:
+			display(arr, n);
+			break;
+		case 4:
+			exit(0);
 		}
 	}
 }
@@ -54,22 +54,22 @@ void insert(int arr[], int *n)
 	scanf("%d", &ele);
 	printf("Enter Position\n");
 	scanf("%d", &pos);
-	for(i=(*n)-1; i>=pos-1; i--)
+	for (i = (*n) - 1; i >= pos - 1; i--)
 	{
-		arr[i+1] = arr[i];
+		arr[i + 1] = arr[i];
 	}
-	arr[pos-1] = ele;
+	arr[pos - 1] = ele;
 	*n = *n + 1;
 }
 
-void delete(int arr[], int *n)
+void delete (int arr[], int *n)
 {
 	int i, pos;
 	printf("Enter the Position to delete the element\n");
 	scanf("%d", &pos);
-	for(i=pos-1; i<(*n)-1; i++)
+	for (i = pos - 1; i < (*n) - 1; i++)
 	{
-		arr[i] = arr[i+1];
+		arr[i] = arr[i + 1];
 	}
 	*n = *n - 1;
 }
@@ -77,7 +77,7 @@ void delete(int arr[], int *n)
 void display(int arr[], int n)
 {
 	int i;
-	for(i=0; i<n; i++)
+	for (i = 0; i < n; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
 }
