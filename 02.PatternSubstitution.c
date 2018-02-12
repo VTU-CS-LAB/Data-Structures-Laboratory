ea61069505cda3d8eafe2f24c19165c02aa6f7a8
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-02-12 17:44:38 +0530

    Formatted

--- 02.PatternSubstitution.c
+++ 02.PatternSubstitution.c
@@ -10,15 +10,15 @@ Support the program with functions for each of the above operations. Don't
 use Built-in functions.
 */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int stringLen(char str[]);
 void stringCat(char dest[], char src[]);
 
 void main()
 {
-	int i=0,j,k=0,flag=0;
+	int i = 0, j, k = 0, flag = 0;
 	char STR[50], PAT[15], REP[15], RESULT[50];
 	printf("Enter String\n");
 	gets(STR);
@@ -26,22 +26,22 @@ void main()
 	gets(PAT);
 	printf("Enter Substitution Key\n");
 	gets(REP);
-	while(STR[i] != '\0')
+	while (STR[i] != '\0')
 	{
-		j=0;
-		while((STR[i+j] == PAT[j]) && (PAT[j] != '\0'))
+		j = 0;
+		while ((STR[i + j] == PAT[j]) && (PAT[j] != '\0'))
 		{
 			j++;
 		}
-		
-		if(PAT[j] == '\0')
+
+		if (PAT[j] == '\0')
 		{
 			flag = 1;
 			RESULT[k] = '\0';
-			stringCat(RESULT,REP);
+			stringCat(RESULT, REP);
 			k = k + stringLen(REP);
 			// Here j will always be equal to string length of pattern.
-			i = i + (j -1);
+			i = i + (j - 1);
 		}
 		else
 		{
@@ -51,7 +51,7 @@ void main()
 	}
 	RESULT[k] = '\0';
 	printf("The result is\n");
-	if(flag)
+	if (flag)
 		puts(RESULT);
 	else
 		printf("Pattern not found\n");
@@ -59,8 +59,8 @@ void main()
 
 int stringLen(char str[])
 {
-	int len=0;
-	while(str[len] != '\0')
+	int len = 0;
+	while (str[len] != '\0')
 	{
 		len++;
 	}
@@ -69,16 +69,16 @@ int stringLen(char str[])
 
 void stringCat(char dest[], char src[])
 {
-	int i=0, j=0;
-	while(dest[i] != '\0')
+	int i = 0, j = 0;
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	// i can be computed using strLength function also. Above while block can be replaced as " i = stringLen(dest); ".
-	
-	while(src[j] != '\0')
+
+	while (src[j] != '\0')
 	{
-		dest[i+j] = src[j];
+		dest[i + j] = src[j];
 		j++;
 	}
 	return;
