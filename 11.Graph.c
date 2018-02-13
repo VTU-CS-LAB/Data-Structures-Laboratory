8ff401abc15374f186d6829268bc2b52dbb2f7e4
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-02-13 16:11:31 +0530

    formatted

--- 11.Graph.c
+++ 11.Graph.c
@@ -6,8 +6,8 @@ b. Print all the nodes reachable from a given starting node in a digraph using
 DFS/BFS method
 */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define size 20
 
 void bfs(int amat[][size], int visited[], int src, int n);
@@ -18,17 +18,17 @@ void main()
 	printf("Enter the no. of cities\n");
 	scanf("%d", &n);
 	printf("Enter the Coef. Adjacency Matrix\n");
-	for(i=0; i<n; i++)
-		for(j=0; j<n; j++)
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
 			scanf("%d", &amat[i][j]);
 	printf("Enter Source\n");
 	scanf("%d", &source);
-	for(i=0; i<n; i++)
+	for (i = 0; i < n; i++)
 		visited[i] = 0;
 	bfs(amat, visited, source, n);
-	for(i=0; i<n; i++)
+	for (i = 0; i < n; i++)
 	{
-		if(visited[i] == 0)
+		if (visited[i] == 0)
 			printf("%d is not reachable\n", i);
 		else
 			printf("%d is reachable\n", i);
@@ -37,15 +37,15 @@ void main()
 
 void bfs(int amat[][size], int visited[], int src, int n)
 {
-	int Q[size], r=0, f=0, u, v;
+	int Q[size], r = 0, f = 0, u, v;
 	visited[src] = 1;
 	Q[r] = src;
-	while(f <= r)
+	while (f <= r)
 	{
 		u = Q[f++];
-		for(v=0; v<n; v++)
+		for (v = 0; v < n; v++)
 		{
-			if((amat[u][v] == 1) && (visited[v] == 0))
+			if ((amat[u][v] == 1) && (visited[v] == 0))
 			{
 				Q[++r] = v;
 				visited[v] = 1;
