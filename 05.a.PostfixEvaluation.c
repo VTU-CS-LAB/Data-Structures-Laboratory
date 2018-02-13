6a57043948e944825d6f77d5cc57ed9d7fc55425
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-02-13 16:08:44 +0530

    Formatted

--- 05.a.PostfixEvaluation.c
+++ 05.a.PostfixEvaluation.c
@@ -5,11 +5,18 @@ a. Evaluation of Suffix expression with single digit operands and operators:
 +, -, *, /, %, ^
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-typedef enum {plus, minus, mul, divi, mod, pwr, eos, operand} precedence;
+typedef enum { plus,
+			   minus,
+			   mul,
+			   divi,
+			   mod,
+			   pwr,
+			   eos,
+			   operand } precedence;
 
 precedence getToken(char postfix[], int *n, char *symbol);
 int eval(char postfix[]);
@@ -28,27 +35,35 @@ void main()
 precedence getToken(char postfix[], int *n, char *symbol)
 {
 	*symbol = postfix[(*n)++];
-	switch(*symbol)
+	switch (*symbol)
 	{
-		case '+':	return plus;
-		case '-':	return minus;
-		case '*':	return mul;
-		case '/':	return divi;
-		case '%':	return mod;
-		case '^':	return pwr;
-		case '\0':	return eos;
-		default:	return operand;
+	case '+':
+		return plus;
+	case '-':
+		return minus;
+	case '*':
+		return mul;
+	case '/':
+		return divi;
+	case '%':
+		return mod;
+	case '^':
+		return pwr;
+	case '\0':
+		return eos;
+	default:
+		return operand;
 	}
 }
 
 int eval(char postfix[])
 {
-	int stack[20], top=-1, n=0, opr1, opr2;
+	int stack[20], top = -1, n = 0, opr1, opr2;
 	precedence token;
 	char symbol;
-	for(token = getToken(postfix, &n, &symbol); token != eos; token = getToken(postfix, &n, &symbol))
+	for (token = getToken(postfix, &n, &symbol); token != eos; token = getToken(postfix, &n, &symbol))
 	{
-		if(token == operand)
+		if (token == operand)
 		{
 			push(stack, &top, symbol - '0');
 		}
@@ -74,13 +89,19 @@ int pop(int stack[], int *top)
 
 int operation(int opr1, int opr2, precedence token)
 {
-	switch(token)
+	switch (token)
 	{
-		case plus:	return opr1+opr2;
-		case minus:	return opr1-opr2;
-		case mul:	return opr1*opr2;
-		case divi:	return opr1/opr2;
-		case mod:	return opr1%opr2;
-		case pwr:	return pow(opr1, opr2);
+	case plus:
+		return opr1 + opr2;
+	case minus:
+		return opr1 - opr2;
+	case mul:
+		return opr1 * opr2;
+	case divi:
+		return opr1 / opr2;
+	case mod:
+		return opr1 % opr2;
+	case pwr:
+		return pow(opr1, opr2);
 	}
 }
