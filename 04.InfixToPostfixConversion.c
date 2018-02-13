8739ba79a8d436d028aae47469ba1758e73ab9d7
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-02-13 15:52:48 +0530

    Formatted

--- 04.InfixToPostfixConversion.c
+++ 04.InfixToPostfixConversion.c
@@ -5,10 +5,19 @@ parenthesized and free parenthesized expressions with the operators: +, -, *,/,
  %(Remainder), ^(Power) and alphanumeric operands.
 */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef enum {lparen, rparen, plus, minus, mul, divi, mod, pwr, eos, operand} precedence;
+typedef enum { lparen,
+			   rparen,
+			   plus,
+			   minus,
+			   mul,
+			   divi,
+			   mod,
+			   pwr,
+			   eos,
+			   operand } precedence;
 
 precedence getToken(char infix[], char *symbol, int *n);
 void convert(char infix[]);
@@ -27,36 +36,46 @@ void main()
 precedence getToken(char infix[], char *symbol, int *n)
 {
 	*symbol = infix[(*n)++];
-	switch(*symbol)
+	switch (*symbol)
 	{
-		case '(':	return lparen;
-		case ')':	return rparen;
-		case '+':	return plus;
-		case '-':	return minus;
-		case '*':	return mul;
-		case '/':	return divi;
-		case '%':	return mod;
-		case '^':	return pwr;
-		case '\0':	return eos;
-		default:	return operand;
+	case '(':
+		return lparen;
+	case ')':
+		return rparen;
+	case '+':
+		return plus;
+	case '-':
+		return minus;
+	case '*':
+		return mul;
+	case '/':
+		return divi;
+	case '%':
+		return mod;
+	case '^':
+		return pwr;
+	case '\0':
+		return eos;
+	default:
+		return operand;
 	}
 }
 
 void convert(char infix[])
 {
-	int stack[20], top = 0, n=0;
+	int stack[20], top = 0, n = 0;
 	int icp[] = {5, 4, 1, 1, 2, 2, 2, 3, 0};
 	int isp[] = {0, 4, 1, 1, 2, 2, 2, 3, 0};
 	precedence token;
 	char symbol;
 	stack[0] = eos;
-	for(token = getToken(infix, &symbol, &n); token != eos; token = getToken(infix, &symbol, &n))
+	for (token = getToken(infix, &symbol, &n); token != eos; token = getToken(infix, &symbol, &n))
 	{
-		if(token == operand)
+		if (token == operand)
 			printf("%c", symbol);
-		else if(token == rparen)
+		else if (token == rparen)
 		{
-			while(stack[top] != lparen)
+			while (stack[top] != lparen)
 			{
 				printToken(pop(stack, &top));
 			}
@@ -64,14 +83,14 @@ void convert(char infix[])
 		}
 		else
 		{
-			while(isp[stack[top]] >= icp[token])
+			while (isp[stack[top]] >= icp[token])
 			{
 				printToken(pop(stack, &top));
 			}
 			push(stack, &top, token);
 		}
 	}
-	while(stack[top] != eos)
+	while (stack[top] != eos)
 	{
 		printToken(pop(stack, &top));
 	}
@@ -89,25 +108,25 @@ precedence pop(int stack[], int *top)
 
 void printToken(precedence token)
 {
-	switch(token)
+	switch (token)
 	{
-		case plus:
-			printf("+");
-			break;
-		case minus:
-			printf("-");
-			break;
-		case mul:
-			printf("*");
-			break;
-		case divi:
-			printf("/");
-			break;
-		case mod:
-			printf("%c", '%');
-			break;
-		case pwr:
-			printf("^");
-			break;
+	case plus:
+		printf("+");
+		break;
+	case minus:
+		printf("-");
+		break;
+	case mul:
+		printf("*");
+		break;
+	case divi:
+		printf("/");
+		break;
+	case mod:
+		printf("%c", '%');
+		break;
+	case pwr:
+		printf("^");
+		break;
 	}
 }
